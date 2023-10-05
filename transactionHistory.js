b21a256cd8a7c99114b052de2edb77fd1d6a3386
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2023-10-05 12:05:00 +0100

    new

--- transactionHistory.js
+++ transactionHistory.js
@@ -2,9 +2,11 @@
 import {ethers} from "./ethers-5.6.esm.min.js"
 import {contractAddress,ABI} from "./constants.js"
 const displayBalance = document.getElementById("see")
-const  balanceScreen = document.getElementById("accountBalance")
+const  balanceScreen = document.getElementById("amounth2")
 const amountIn = document.getElementById("in")
 const amountOut = document.getElementById("out")
+const dropdown = document.getElementById("button")
+dropdown.onclick = toggleDropdown;
 displayBalance.onclick = display
 
  async function display() {
@@ -17,12 +19,21 @@ displayBalance.onclick = display
         const Contract = new ethers.Contract(contractAddress,ABI,signer)
         const userBalance = await Contract.balanceOfUser()
         console.log(ethers.utils.formatEther(userBalance))
-        balanceScreen.innerHTML = "BALANCE: " + userBalance +  " ether"
+        balanceScreen.innerHTML = "BALANCE: " + ((userBalance)/1e18).toFixed(2) +  " ether"
         const totalAmountOut = await Contract.totalAmountOut()
         const totalAmountIn = await Contract.totalAmountIn()
-        const totalIn = parseFloat(ethers.utils.formatEther(totalAmountOut))
-        const totalOut = parseFloat(ethers.utils.formatEther(totalAmountIn))
+        const totalIn = parseFloat(ethers.utils.formatEther(totalAmountIn))
+        const totalOut = parseFloat(ethers.utils.formatEther(totalAmountOut))
         amountIn.innerHTML = "IN: " + totalIn.toFixed(2) + "ether"
-        amountOut.innerHTML = "OUT" + totalOut.toFixed(2) + "ether"
+        amountOut.innerHTML = "OUT: " + totalOut.toFixed(2) + "ether"
      }
+}
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdown-content");
+  if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+  } else {
+      dropdownContent.style.display = "none";
+  }
 }
